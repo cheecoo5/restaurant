cefaad9ec783d899c25131014a2a346668f02a77
Deysi19 <deysimaria_19@hotmail.com>
2016-09-24 20:38:56 -0500

    Interfacez

---
+++ WinForms/WinForms/POSTRE.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms
+{
+    public partial class POSTRE : Form
+    {
+        postres P = new postres();
+        public POSTRE()
+        {
+            InitializeComponent();
+        }
+
+        private void POSTRE_Load(object sender, EventArgs e)
+        {
+            P.conectar();
+            mostrarDatos();
+
+
+        }
+
+        private void bu_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void mostrarDatos()
+        {
+            P.consulta("select*from POSTRE", "POSTRE");
+            dataGridView1.DataSource = P.ds.Tables["POSTRE"];
+        }
+
+
+    }
+}
