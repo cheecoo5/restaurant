25d81799b27dad7236836e1ec363ddb861886b40
Deysi19 <deysimaria_19@hotmail.com>
2016-09-24 21:43:18 -0500

    mas cambios tengo sueño

--- WinForms/WinForms/POSTRE.cs
+++ WinForms/WinForms/POSTRE.cs
@@ -28,7 +28,16 @@ namespace WinForms
 
         private void bu_Click(object sender, EventArgs e)
         {
-
+            string agregar = "insert into POSTRE values(" + txtid.Text + ",'" + txtnombre.Text + "'," + txtprecio.Text + ",'" + txtexistencia.Text + "')";
+            if (P.agregar(agregar))
+            {
+                MessageBox.Show("Datos agregados");
+                mostrarDatos();
+            }
+            else
+            {
+                MessageBox.Show("Error al agregar");
+            }
         }
         public void mostrarDatos()
         {
@@ -36,6 +45,53 @@ namespace WinForms
             dataGridView1.DataSource = P.ds.Tables["POSTRE"];
         }
 
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
 
+        private void btnmodificar_Click(object sender, EventArgs e)
+        {
+            string actualizar = "ID=" + txtid.Text + ",NOMBRE='" + txtnombre.Text + "',PRECIO=" + txtprecio.Text + ",EXISTENCIA='" + txtexistencia + "'";
+            if (P.modificar("POSTRE", actualizar, "ID=" + txtid.Text))
+            {
+                MessageBox.Show("Datos Actualizados");
+                mostrarDatos();
+            }
+            else
+            {
+                MessageBox.Show("Error al Actualizar");
+            }
+        }
+
+        private void btnbajas_Click(object sender, EventArgs e)
+        {
+
+            if (P.eliminar("POSTRE", "ID=" + txtid.Text))
+            {
+                MessageBox.Show("Se elimino");
+                txtid.Text = "";
+                txtnombre.Text = "";
+                txtprecio.Text = "";
+                txtexistencia.Text = "";
+
+                mostrarDatos();
+            }
+            else
+            {
+                MessageBox.Show("No se pudo eliminar");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow grid = dataGridView1.Rows[e.RowIndex];
+
+            txtid.Text = grid.Cells[0].Value.ToString();
+            txtnombre.Text = grid.Cells[1].Value.ToString();
+            txtprecio.Text = grid.Cells[2].Value.ToString();
+            txtexistencia.Text = grid.Cells[3].Value.ToString();
+
+        }
     }
 }
